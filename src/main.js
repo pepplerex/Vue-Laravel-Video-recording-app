6a9ee86fcd8caa7045720b8e1af3e317bdacdb82
pepplerex <pepplerex01@gmail.com>
2024-01-07 21:14:22 +0100

    minor update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-import "./App.css";
+import "./App.less";
 import Dashboard from "./view/App.vue";
 import App from "./App.vue";
 
